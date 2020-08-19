c0a04110d8febcdee754829c3425062178c27714
Dominik-97 <58784420+Dominik-97@users.noreply.github.com>
2020-08-19 13:43:24 +0200

    Pokračování na prezentaci

---
+++ MindMaps/Zkoumání.mm
@@ -0,0 +1,24 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1597833653794" ID="ID_593930373" MODIFIED="1597834402169" TEXT="Sv&#x11b;&#x159;ensk&#xfd; fond">
+<node CREATED="1597833705846" HGAP="-21" ID="ID_1562520361" MODIFIED="1597834488293" POSITION="right" TEXT="D&#x11b;dick&#xe9; pr&#xe1;vo, optika 1" VSHIFT="62">
+<cloud/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1597833756153" HGAP="-176" ID="ID_438574761" MODIFIED="1597834500946" TEXT="V&#xfd;kladov&#xe1; metoda" VSHIFT="-123">
+<node CREATED="1597834169312" HGAP="29" ID="ID_1780623477" MODIFIED="1597834525117" TEXT="&#xda;mysl z&#xe1;konod&#xe1;rce" VSHIFT="-6"/>
+<node CREATED="1597834175637" HGAP="22" ID="ID_607613881" MODIFIED="1597834521481" TEXT="Sm&#x11b;&#x159;ov&#xe1;n&#xed; z&#xe1;konod&#xe1;rce" VSHIFT="21"/>
+<node CREATED="1597834208403" HGAP="32" ID="ID_1716110947" MODIFIED="1597834519850" TEXT="Korelace a kauzalita" VSHIFT="19"/>
+<node CREATED="1597834251813" HGAP="29" ID="ID_1032354683" MODIFIED="1597834515799" TEXT="Principy pr&#xe1;va" VSHIFT="20"/>
+</node>
+</node>
+<node CREATED="1597833802571" HGAP="-30" ID="ID_1235331371" MODIFIED="1597834468039" POSITION="left" TEXT="D&#x11b;dick&#xe9; pr&#xe1;vo, optika 2" VSHIFT="61">
+<cloud/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1597833816864" HGAP="-127" ID="ID_1631200784" MODIFIED="1597834482562" TEXT="&#xda;myslov&#xe1; metoda" VSHIFT="-125">
+<node CREATED="1597833963338" HGAP="14" ID="ID_1022138618" MODIFIED="1597834075509" TEXT="Form&#xe1;ln&#xed; v&#xfd;klad" VSHIFT="-27"/>
+<node CREATED="1597833971679" HGAP="27" ID="ID_663497449" MODIFIED="1597834082039" TEXT="Vz&#xe1;jemn&#xe9; vztahy"/>
+<node CREATED="1597833975935" HGAP="13" ID="ID_228697755" MODIFIED="1597834087795" TEXT="Principy sv&#x11b;&#x159;ensk&#xe9;ho fondu, pr&#xe1;va a logiky" VSHIFT="28"/>
+</node>
+</node>
+</node>
+</map>
